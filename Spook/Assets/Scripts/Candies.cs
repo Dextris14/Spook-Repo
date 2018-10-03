c25b8dcc97a9e5e231f55444056bfbc71739feac
Alex Wise <19.alex.wise@ksd.org>
2018-10-03 08:38:55 -0700

    Added the first scripts

---
+++ Spook/Assets/Scripts/Candies.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Candies : MonoBehaviour {
+    public int CandyCount = 0;
+    public Text CandyCounter;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CandyCounter.text = "" + CandyCount;
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Candy")
+        {
+            CandyCount++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
