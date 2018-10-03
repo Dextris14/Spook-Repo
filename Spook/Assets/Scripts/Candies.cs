099fef874d03846b15783bba7ca96badd6d4b383
Alex Wise <19.alex.wise@ksd.org>
2018-10-03 09:38:20 -0700

    Added basic Collect Candy --> Collect Pumpkin --> Next Level

--- Spook/Assets/Scripts/Candies.cs
+++ Spook/Assets/Scripts/Candies.cs
@@ -6,9 +6,12 @@ using UnityEngine.UI;
 public class Candies : MonoBehaviour {
     public int CandyCount = 0;
     public Text CandyCounter;
+    public GameObject Pumpkin;
+    public GameObject Transition;
 	// Use this for initialization
 	void Start () {
-		
+        Pumpkin.SetActive(false);
+        Transition.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -21,6 +24,17 @@ public class Candies : MonoBehaviour {
         if(collision.gameObject.tag == "Candy")
         {
             CandyCount++;
+            Destroy(collision.gameObject);
+        }
+
+        if(CandyCount >=10)
+        {
+            Pumpkin.SetActive(true);
+        }
+
+        if(collision.gameObject.tag == "Pumpkin")
+        {
+            Transition.SetActive(true);
             Destroy(collision.gameObject);
         }
     }
