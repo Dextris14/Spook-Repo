ba9f5d018b4bca0b845e79fdade582e46afc02cc
Aunoris <wayovercomplicatedemail@gmail.com>
2018-10-04 18:47:36 -0700

    Finished adding basic art and enemies

--- Spook/Assets/Scripts/EnemyMovement.cs
+++ Spook/Assets/Scripts/EnemyMovement.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
-    public Vector3 Relation = new Vector3();
+    public Vector2 Relation = new Vector2();
     public float moveSpeed = 1f;
     public int Type;
+    public GameObject Player;
+    public float Timer = 0;
 	// Use this for initialization
 	void Start () {
-        if(Type == 1)
+        Player = GameObject.FindWithTag("Player");
+        if (Type == 1)
         {
             moveSpeed = 4;
         }
@@ -18,7 +21,11 @@ public class EnemyMovement : MonoBehaviour {
         }
         if (Type == 3)
         {
-            moveSpeed = 2;
+            moveSpeed = 40;
+        }
+        if (Type == 4)
+        {
+            moveSpeed = 25;
         }
     }
 	
@@ -26,21 +33,42 @@ public class EnemyMovement : MonoBehaviour {
 	void Update () {
         if(Type == 1)
         {
-            Vector3 PlayerPosition = new Vector3(GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().position.x, GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().position.y, 0);
-            Vector3 Relation = PlayerPosition - transform.position;
+            Vector2 Relation = Player.GetComponent<Rigidbody2D>().position - GetComponent<Rigidbody2D>().position;
             GetComponent<Rigidbody2D>().velocity = Relation.normalized * moveSpeed;
         }
         if (Type == 2)
         {
-            Vector3 PlayerPosition = new Vector3(GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().position.x, GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().position.y, 0);
-            Vector3 Relation = PlayerPosition.normalized - transform.position.normalized;
-            GetComponent<Rigidbody2D>().velocity = Relation * moveSpeed;
+            Vector2 Relation = (Player.GetComponent<Rigidbody2D>().position + Player.GetComponent<Rigidbody2D>().velocity) - GetComponent<Rigidbody2D>().position;
+            GetComponent<Rigidbody2D>().velocity = Relation.normalized * moveSpeed;
         }
         if (Type == 3)
         {
-            Vector3 PlayerPosition = new Vector3(GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().position.x, GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().position.y, 0);
-            Vector3 Relation = PlayerPosition - transform.position.normalized;
-            GetComponent<Rigidbody2D>().velocity = Relation * moveSpeed;
+            Timer += Time.deltaTime;
+            if(Timer > 1.2)
+            {
+                Vector2 Relation = Player.GetComponent<Rigidbody2D>().position - GetComponent<Rigidbody2D>().position;
+                GetComponent<Rigidbody2D>().velocity = Relation.normalized * moveSpeed;
+                if(Timer >1.3)
+                {
+                    Timer = 0;
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                }
+            }
+        }
+        if (Type == 4)
+        {
+            Timer += Time.deltaTime;
+            if (Timer >= 1)
+            {
+                Vector2 Relation = Player.GetComponent<Rigidbody2D>().position - GetComponent<Rigidbody2D>().position;
+                GetComponent<Rigidbody2D>().velocity = Relation.normalized * moveSpeed;
+                Timer = 0;
+                GetComponent<Rigidbody2D>().drag = 0;
+            }
+            if (Timer >= 0.3)
+            {
+                GetComponent<Rigidbody2D>().drag = 5;
+            }
         }
     }
 }
